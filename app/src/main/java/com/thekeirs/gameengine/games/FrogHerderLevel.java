a98e93101317bbb6c78de67a5aad01717c9cbd65
wurzel <wurzel@fb.com>
2017-01-11 22:48:21 -0800

    Update build tools to 25.0.2, minor code cleanup

--- app/src/main/java/com/thekeirs/gameengine/games/FrogHerderLevel.java
+++ app/src/main/java/com/thekeirs/gameengine/games/FrogHerderLevel.java
@@ -19,25 +19,19 @@ public class FrogHerderLevel extends GameLevel {
     final int HEIGHT = 900;
     final int NUM_FROGS = 5;
 
-    public List<GameObject> frogs;
-    public Scene scene;
-
     public FrogHerderLevel() {
         super();
     }
 
     public void setup() {
-        scene = new SolidColorScene("#20c0ff");
-        // scene = new BackgroundImageScene(mManager, R.raw.stupid_background);
-        mManager.setScene(scene);
         mManager.setWorldScreenSize(WIDTH, HEIGHT);
-
-        frogs = new ArrayList<>();
+        Scene scene = new SolidColorScene("#20c0ff");
+        // scene = new BackgroundImageScene(mManager, R.raw.my_background);
+        mManager.setScene(scene);
 
         for (int i = 0; i < NUM_FROGS; ++i) {
             int frogSize = Rand.between(80, 150);
             GameObject frog = new FrogSprite("frog" + i, Rand.between(0, WIDTH - 30), Rand.between(0, HEIGHT - 30), frogSize, frogSize);
-            frogs.add(frog);
             mManager.addObject(frog);
         }
     }
